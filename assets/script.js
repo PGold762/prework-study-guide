d87556b77485de370e8ebaf8d49e3fde0d1d173a
PGold <pglz762@gmail.com>
2023-06-14 13:51:20 -0500

    added JS file and attached to index page and made further changes to JS file

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,31 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for(var x = 0; x <topics.length; x++) {
-    console.log(topics[x]);
-}
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
+  console.log('Which topic should we study first?');
+  selectTopic();
+  
+  function selectTopic() {
+    if (randomTopic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+  }
 
 /*
 if (topic === 'HTML') {
@@ -21,7 +42,10 @@ if (topic === 'HTML') {
 */
 
 
-var shapes = ["triangle", "square", "pentagon", "circle"];
+/* var shapes = ["triangle", "square", "pentagon", "circle"];
 for(var x = 0; x < shapes.length; x++) {
  console.log(shapes[x]);
-}
+} */
+
+listTopics()
+selectTopic()
